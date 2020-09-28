a10554db2d27c86e270fd357872a74c75d74ac74
venkatreddymallidi <70442143+venkatreddymallidi@users.noreply.github.com>
2020-09-28 09:01:06 +0530

    using sqrt method

---
+++ squareroot.py
@@ -0,0 +1,3 @@
+import math #math module is invocated by using import method to use sqrt func
+n=int(input("enter square of a no:"))
+print(int(math.sqrt(n)))#square method returns result in float so type casted into int
