ff09d7aaf7e3a2206c2ba0161beceda9fc3da07a
venkatreddymallidi <70442143+venkatreddymallidi@users.noreply.github.com>
2020-09-28 08:43:00 +0530

    Add files via upload
    function

---
+++ reverse of a no.py
@@ -0,0 +1,12 @@
+def reverse(n):
+    rev=0
+    while n>0:
+        lastno=n%10
+        rev=rev*10+lastno
+        n=n//10
+    
+    print('reverse of given number:',rev)
+n=int(input("enter a no:"))
+reverse(n)
+
+        
