e2e4443152d5dadac31557fb2e55c065b6e0d9e9
venkatreddymallidi <70442143+venkatreddymallidi@users.noreply.github.com>
2020-09-29 14:46:33 +0530

    ifelse

--- leapyear.py
+++ leapyear.py
@@ -3,4 +3,4 @@ year=int(input("enter a year"))
 if year%400==0 or (year%4==0 and year%100!=0):#1996 and 2004 is divisible by 4 and divisible by 100 so leap years  2100 not a leap year but divisible by 4 divisible by 100 so leap year divisible by 4 not divisible by 100
     print("leap year")
 else:
-    print("non leap year")
+    print("non leap year ")
