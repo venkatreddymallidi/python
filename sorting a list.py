59638f66f05c87c700c5d1e06cb53b414608258a
venkatreddymallidi <70442143+venkatreddymallidi@users.noreply.github.com>
2020-09-30 20:23:22 +0530

    In ascending and descending order

---
+++ sorting a list.py
@@ -0,0 +1,10 @@
+a=[99,56,2,1,38,29,10,21,76,55,100]
+b=a#assigning "a" list to "b" i.e,a and b has memory location so modification in a reflect in b 
+a.sort()# this method arrange all the elements in ascending order
+print(a)
+a.sort(reverse=True)#first sorts and then reverse we get descending order
+print(a)
+a.sort()#second method for desecending order
+b.reverse()
+print(b)
+
