c5bef4311942c6497e560a380d49107e9736af2c
venkatreddymallidi <70442143+venkatreddymallidi@users.noreply.github.com>
2020-09-30 19:45:25 +0530

    Add files via upload

---
+++ removing duplicates from list.py
@@ -0,0 +1,6 @@
+a=[2,4,6,2,1,10,1,2,4,9,3,2,6,5] # declaring a list
+b=[]# declaring a empty list
+for i in a: #accessing list a and assigning elements to i
+    if i not in b:
+        b.append(i)#append method in list add value to last position
+print(b)
