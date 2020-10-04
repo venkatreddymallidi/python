15e3e5890d3044d8b565799a6c7f359458aded83
venkatreddymallidi <70442143+venkatreddymallidi@users.noreply.github.com>
2020-10-04 11:52:33 +0530

    practice on lists

---
+++ testing.py
@@ -0,0 +1,59 @@
+##lst=[1,2,3]
+##for v in range(len(lst)):
+##    lst.insert(1,lst[v])
+##print(lst)
+
+##var=0
+##while var<6:
+##    var +=1
+##    if var %2==0:
+##        continue
+##    print("#")
+
+##z=10
+##y=0
+##x=y<z and z>y or y>z and z<y
+##print(x)
+
+##t=[[3-i for i in range(3)]for j in range(3)]
+##s=0
+##for i in range(3):
+##    s+=t[i][i]
+##print(s)
+
+##lst=[3,1,-2]
+##print(lst[lst[-1]])
+
+##i=0
+##while i<=5:
+##    i +=1
+##    if i%2==0:
+##        break
+##    print("*")
+
+
+##for i in range(1):
+##    print('#')
+##else :
+##    print("#")
+##    
+
+##lst1=[1,2,3]
+##lst2=[]
+##for v in lst1:
+##    lst2.insert(0,v)
+##print(lst2)
+
+##vals=[0,1,2]
+##vals[0],vals[2]=vals[2],vals[0]
+##print(vals==vals.sort(reverse=True))
+###print(vals)
+
+
+##lst=[i for i in range(-1,2)]
+##
+
+nums=[1,2,3]
+vals=nums[-1:-2:-1]
+print(nums)
+print(vals)
