8d192283ddd27fdb60c49b453111c7624cbbea13
venkatreddymallidi <70442143+venkatreddymallidi@users.noreply.github.com>
2020-12-19 05:24:45 +0530

    basic oop concept

---
+++ Opp1.py
@@ -0,0 +1,9 @@
+class Wishing: #defining a class (class Class name)
+    def display(self):
+        print("hello")
+    def greeting(self):
+        print("good morning")
+
+w1=Wishing()  #w1 is a object which is instance(example) of class
+w1.display()  #any normal method(function) is called by using object of that class
+w1.greeting()
